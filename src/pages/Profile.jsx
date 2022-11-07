0ca570ae341823d6498b7cc8ab97e4a992827855
moha6248 <dk98@outlook.dk>
2022-11-07 23:53:33 +0100

    UserProfile created

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Profile from "../components/Profile";
+import UserProfile from "../components/UserProfile";
 
 const ProfilePage = () => {
   return (
     <>
         <Profile />
+        <UserProfile />
     </>
   );
 };
