28cd1ac6048266ab117949a3a6d2abaed15a95e5
moha6248 <dk98@outlook.dk>
2022-11-07 01:08:57 +0100

    Ticket frontend created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,35 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom';
 //import Home from "../components/Home";
-import bgImg from "../images/backGround.png";
+import bgImg from "../images/party.jpg";
+
+import Ticket from "../components/Ticket";
+import "../pages/pages.css";
 
 
 const HomePage = () => {
 
+  const navigate = useNavigate();
+  const navigateTicket = () => {
+    navigate('/Ticket');
+  };
 
   return (
     <>
-      <h1>Home</h1>
-      <img style={{margin:"0% 10%"}}src={bgImg} alt="Background"/>
+    
+    <div className="home-body">
+      
+
+      <img className="home-background-img" src={bgImg} alt="Background"/>
+      <h1 >Browse all tickets</h1>
+
+          <div className="home-ticket" onClick={navigateTicket}>
+          <Ticket /> 
+          </div>
+          <div className="home-ticket" onClick={navigateTicket}>
+          <Ticket /> 
+          </div>
+    </div>
     </>
   );
 }
--- src/pages/Ticket.jsx
+++ src/pages/Ticket.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 //import Home from "../components/Home";
 import bgImg from "../images/backGround.png";
-
+import Ticket from "../components/Ticket";
+import "../pages/pages.css";
+import Seller from "../components/Seller";
 
 const TicketPage = () => {
 
-
   return (
-    <>
-      <h1>Ticket View</h1>
-      <img style={{margin:"0% 10%"}}src={bgImg} alt="Background"/>
+    
+    <><h1>Ticket View</h1>
+    <div className="single-ticket-page">
+      <div className="single-ticket">
+      <Ticket /> 
+      </div>
+      <div>
+      <Seller />
+      </div>
+    </div>
     </>
   );
 }
