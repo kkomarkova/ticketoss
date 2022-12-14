132ecbd108293dcc444a0b7a94cd6bd79dd6fe4f
Katerina Komarkova <58290791+kkomarkova@users.noreply.github.com>
2022-12-14 19:39:05 +0100

    sentry setup

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ import { Provider } from "react-redux"
 import store from "./store/store";
 import { BrowserRouter } from "react-router-dom";
 import setUpInterceptors from "./store/services/setupInterceptors";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
+//Initialize Sentry
+Sentry.init({
+  dsn: "https://bf93100dbe2a424690bf5a167a13f2ad@o4504327890206720.ingest.sentry.io/4504327893745664",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const container = document.getElementById("root");
 const root = createRoot(container);
